99efb959c8dd3cf0c0e9958a899648b7f83dd16e
kundanweo <kundan@webexpertsonline.info>
2018-10-08 10:42:00 +0530

    add something

--- ishan.php
+++ ishan.php
@@ -1,10 +1,11 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+ work idk ishan
 
 echo phpinfo();
 
